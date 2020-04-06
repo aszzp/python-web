2ca0cb7d62f81f73fdcea7060023402c6b8c0129
aszzp <eastzzp@gmail.com>
2020-04-06 23:05:19 +0800

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,25 +1,2 @@
 console.log($('div'));
-// window.onload = function(){
-//     console.log("hello, js has been loaded!");
-//     // var test = $('[class="_2fkQahkNYrxitTMw9Op8tR"] div.shadow');
-//     var test = $('[class="xuexi"]');
-//     console.log(test)
-// }
-
-// qrcode = $('[class="login_qrcode_content"] img');
-// console.log(qrcode.src);
-// alert(test.text());
-// var aaaa= "xixai";
-// function callb(response)
-// {
-// 	console.log('收到来自后台的回复：' +aaaa+ response);
-// }
-
-// chrome.runtime.sendMessage({greeting: '你好1，我是content-script呀，我主动发消息给后台！'}, callb);
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
-// {
-// 	// console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
-// 	if(request.cmd == 'test') alert(request.value);
-// 	sendResponse('我收到了你的消息！');
-// });
+alert('ss');
